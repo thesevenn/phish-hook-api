13bdbd1af380a8043e31b7d1b117302c8c58c0eb
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-17 20:42:18 +0530

    feat: add logging config for API

---
+++ app/config/logging.py
@@ -0,0 +1,67 @@
+import os
+import logging
+from logging.config import dictConfig
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(asctime)s | %(name)s | %(message)s",
+            "use_colors": None,
+        },
+        "detailed": {
+            "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+        "file_requests": {
+            "class": "logging.FileHandler",
+            "formatter": "detailed",
+            "filename": f"{LOG_DIR}/requests.log",
+            "mode": "a",
+        },
+        "file_errors": {
+            "class": "logging.FileHandler",
+            "formatter": "detailed",
+            "filename": f"{LOG_DIR}/errors.log",
+            "mode": "a",
+        },
+        "file_detections": {
+            "class": "logging.FileHandler",
+            "formatter": "detailed",
+            "filename": f"{LOG_DIR}/detections.log",
+            "mode": "a",
+        },
+    },
+    "loggers": {
+        "uvicorn": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "uvicorn.error": {
+            "level": "ERROR",
+            "handlers": ["console", "file_errors"],
+            "propagate": True,
+        },
+        "uvicorn.access": {
+            "level": "INFO",
+            "handlers": ["console", "file_requests"],
+            "propagate": False,
+        },
+        "detection": {
+            "level": "INFO",
+            "handlers": ["console", "file_detections"],
+            "propagate": False,
+        },
+    }
+
+}
